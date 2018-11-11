e3caf93e4e6e356f31b5fe30fd1405e6b04fbbef
daniel <daniel.soler@e-campus.uab.cat>
2018-11-12 00:35:23 +0100

    Add  user generator class and similarity retrival on top of it

--- users/recommender.py
+++ users/recommender.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import argparse
 import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.model_selection import cross_val_score
+import user_generator as gen
 
 EVENT = 'Events'
 AV_RATE = "Average rating"
 COUNTS = "Counts"
 SIMILARITY = "similarity"
+RATES = 'Rates'
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
@@ -31,6 +35,16 @@ class Event_DB():
         similars = general_df.nlargest(10, [SIMILARITY])[EVENT].tolist()[0:number]
         return similars
 
+    def retrieve_similar_users(self, user):
+        users = self.df.drop([EVENT, AV_RATE, COUNTS], axis=1).transpose()
+        clf = KMeans(n_clusters=100, random_state=0).fit(users)
+        users["cluster"] = clf.labels_
+        user_cluster = clf.predict(user.retrieve_df().drop(EVENT, axis=1)[RATES].values.reshape(1,-1))
+        return (users[users["cluster"] == user_cluster[0]].index.values)
+
+
+    def retrieve_events(self):
+        return self.df[EVENT]
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Recommend n similar events")
@@ -47,5 +61,10 @@ if __name__ == "__main__":
     args = parse_args()
     event_db = Event_DB(pd.read_csv("random_users.csv"))
     similar_events = event_db.retrieve_similar_events(args.event, args.number)
-    print("The list of {} similars events to {} is:\n".format(args.number, args.event))
+    print("The list of {} similars events to {} is:".format(args.number, args.event))
     print("\n".join(similar_events))
+    user = gen.create_user("Dani", event_db.retrieve_events())
+    similar_users = event_db.retrieve_similar_users(user)
+    print("\nThe next users are similar to {}:".format(user.name))
+    print("\n".join(similar_users))
+

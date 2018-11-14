e9e1f39757a6d3ab971a8aa2ec762674f41a4e66
Philippe van Amerongen <philippe.vanamerongen@gmail.com>
2018-11-14 12:24:07 +0100

    Tiny tweeks and added user_seeder

---
+++ scripts/user_seeder.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_mongoengine import MongoEngine
+from app.User import User
+from faker import Faker
+
+app = Flask(__name__)
+app.config['MONGODB_DB'] = 'bcneventer'
+app.config['MONGODB_HOST'] = "mongodb://localhost:27017/bcneventer"
+
+db = MongoEngine(app)
+
+faker = Faker()
+
+
+User.drop_collection()
+
+n = 10
+
+# Test user
+user = User.create('test@test.com', '12345678')
+
+for i in range(n):
+    user = User.create(faker.safe_email(), faker.password())

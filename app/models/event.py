ed24ed4951867a57e65dede7c42297eedde4ca1b
stufi04 <st.ivanov44@gmail.com>
2018-11-25 20:24:40 +0100

    added a slider with recommended events

--- app/models/event.py
+++ app/models/event.py
@@ -1,20 +1,29 @@
-from .base import Base
+import mongoengine
 
+from app.models.category import Category
+from .base import Base
 from datetime import datetime
 
 class Event(Base):
-    name = ""
-    location = ""
-    coordinates = (0, 0)
-    time = ""
+    name = mongoengine.StringField()
+    description = mongoengine.StringField()
+    coordinates = mongoengine.PointField()
+    address = mongoengine.StringField()
+    date_time = mongoengine.DateTimeField()
+    category_ids = mongoengine.ListField(mongoengine.ReferenceField(Category))
 
-    def __init__(self, name, coordinates, unixTime, *args, **values):
+    def __init__(self, name='', description='', coordinates=(0, 0), location='', date_time=0, category_ids=None, *args, **values):
         super().__init__(*args, **values)
         self.name = name
+        self.description = description
         self.coordinates = coordinates
+        self.location = location
+        self.date_time = date_time
+        self.category_ids = category_ids
 
         try:
-            timeStamp = int(unixTime)/1000 #divide to get second from milliseconds
-            self.time = datetime.utcfromtimestamp(timeStamp).strftime('%Y-%m-%d %H:%M:%S')
+            timeStamp = int(date_time) / 1000  # divide to get second from milliseconds
+            self.readable_time = datetime.utcfromtimestamp(timeStamp).strftime('%a, %d %b %Y, %H:%M')
         except:
-            self.time = "unknown"
+            self.readable_time = "unknown"
+
